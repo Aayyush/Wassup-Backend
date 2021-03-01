aa7920cb2e09f5f47e4e56719d24dc8b83d4a545
Aayush Gupta <aayush.gupta@bison.howard.edu>
2021-03-01 16:54:19 -0500

    Clean up.

--- wassup/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Criteria, Event 
-
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(Criteria)
-admin.site.register(Event)
